f6fa042c00326a50b2e5716b5401d2b64583e5e5
Arbai-Ilyas <arbai.ilyas.dev@gmail.com>
2026-09-20 22:59:43 +0700

    update-logo

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+document.addEventListener("contextmenu", (e) => {
+  if (e.target instanceof HTMLImageElement) {
+    e.preventDefault();
+  }
+});
+
+createApp(App).mount("#app");
