f15be0a1c38c36146986f97f8b1ba60c0a8b0b19
Arbai-Ilyas <arbai.ilyas.dev@gmail.com>
2026-09-20 23:05:31 +0700

    update-img-js

--- src/main.js
+++ src/main.js
@@ -7,5 +7,26 @@ document.addEventListener("contextmenu", (e) => {
     e.preventDefault();
   }
 });
+let longPressTimer;
+document.addEventListener(
+  "touchstart",
+  (e) => {
+    if (e.target instanceof HTMLImageElement) {
+      longPressTimer = setTimeout(() => {
+        e.preventDefault();
+      }, 500);
+    }
+  },
+  { passive: false },
+);
+document.addEventListener("touchend", () => {
+  clearTimeout(longPressTimer);
+});
+document.addEventListener("touchmove", () => {
+  clearTimeout(longPressTimer);
+});
+document.addEventListener("touchcancel", () => {
+  clearTimeout(longPressTimer);
+});
 
 createApp(App).mount("#app");
